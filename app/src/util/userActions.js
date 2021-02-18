f7bf38453d116905153617346fb1857da84a7c2a
Mike Tahani <me@miketahani.com>
2021-02-17 21:53:17 -0800

    extract user actions

---
+++ app/src/util/userActions.js
@@ -0,0 +1,14 @@
+import { fetchPost } from './fetchPost'
+
+import { BASE_REST_API_URL } from '../config'
+
+export const getUserList = async () => {
+  const res = await fetch(`${BASE_REST_API_URL}/user/list`)
+  const users = await res.json()
+  users.rows = users.rows.sort((a, b) => b.id - a.id)
+  return users
+}
+
+export const removeUser = async userId => {
+  await fetchPost(`${BASE_REST_API_URL}/user/remove`, {id: userId})
+}
