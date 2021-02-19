cc5a065cd45f9871e1687a49d5b429e7cf4ffa4b
Mike Tahani <me@miketahani.com>
2021-02-18 16:38:02 -0800

    remove modal logic

--- app/src/components/UserList/UserListWebsocket.js
+++ app/src/components/UserList/UserListWebsocket.js
@@ -11,7 +11,6 @@ import { WS_NOTIFICATIONS_URL } from '../../config'
 
 export function UserListWebsocket () {
   const [users, setUsers] = useState(null)
-  const [modalOpen, setModalOpen] = useState(false)
 
   const requestUserList = async () => {
     try {
@@ -21,40 +20,13 @@ export function UserListWebsocket () {
     }
   }
 
-  const handleRemoveUser = async userId => {
-    try {
-      await removeUser(userId)
-      setModalOpen(false)
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
   // Subscribe to updates and reload the user list when a change is detected
   const ws = useWebSocket(WS_NOTIFICATIONS_URL, requestUserList)
 
   // Hydrate initial user list
   useEffect(() => { requestUserList() }, [])
 
-  const showDeleteWarningModal = userId => {
-    setModalOpen({
-      userId,
-      buttons: [
-        ['DELETE', () => handleRemoveUser(userId)],
-        ['Cancel', () => setModalOpen(false)]
-      ]
-    })
-  }
-
   return (
-    <>
-      <UserList
-        users={users}
-        subtitle="push via websocket"
-        onRemoveUser={showDeleteWarningModal}
-      />
-
-      {!!modalOpen && <DeleteUserModal {...modalOpen} />}
-    </>
+    <UserList users={users} subtitle="push via websocket" />
   )
 }
