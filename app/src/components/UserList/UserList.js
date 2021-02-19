3d7f3b36f8c28fbbd6361ed62a8f5a605c737938
Mike Tahani <me@miketahani.com>
2021-02-18 16:38:43 -0800

    move modal logic to base UserList

--- app/src/components/UserList/UserList.js
+++ app/src/components/UserList/UserList.js
@@ -1,23 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { User } from './User'
 import { UserListHeader } from './UserListHeader'
+import { DeleteUserModal } from './DeleteUserModal'
 
-export const UserList = ({ users, subtitle, onRemoveUser = null }) => (
-  <div>
-    <UserListHeader
-      subtitle={subtitle}
-      retrieveDate={users && users.meta.retrieveDate}
-    />
+import { removeUser } from '../../util/userActions'
 
-    {(!users || !users.rows.length) && 'No users'}
+export const UserList = ({ users, subtitle, onRemoveUser = null }) => {
+  const [modalOpen, setModalOpen] = useState(false)
 
-    {users && users.rows.map(user =>
-      <User
-        user={user}
-        onRemoveUser={onRemoveUser}
-        key={user.id}
-      />
-    )}
-  </div>
-)
+  const handleRemoveUser = async userId => {
+    try {
+      await removeUser(userId)
+      setModalOpen(false)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const showDeleteWarningModal = userId => {
+    setModalOpen({
+      userId,
+      buttons: [
+        ['DELETE', () => handleRemoveUser(userId)],
+        ['Cancel', () => setModalOpen(false)]
+      ]
+    })
+  }
+
+  return (
+    <>
+      <div>
+        <UserListHeader
+          subtitle={subtitle}
+          retrieveDate={users && users.meta.retrieveDate}
+        />
+
+        {(!users || !users.rows.length) && 'No users'}
+
+        {users && users.rows.map(user =>
+          <User
+            user={user}
+            onRemoveUser={() => showDeleteWarningModal(user.id)}
+            key={user.id}
+          />
+        )}
+      </div>
+
+      {!!modalOpen && <DeleteUserModal {...modalOpen} />}
+    </>
+  )
+}
